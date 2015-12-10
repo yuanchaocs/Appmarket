e45c8cb8892075046b0a998640101b81573961ef
袁小超 <yuanchaocs@163.com>
2015-12-11 00:26:08 +0800

    测试提交

--- app/src/main/java/com/androidy/app/appmarket/MainActivity.java
+++ app/src/main/java/com/androidy/app/appmarket/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.androidy.app.lib.BaseActivity;
 
@@ -75,5 +76,9 @@ public class MainActivity extends BaseActivity
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void hitButton(View view) {
+        openSingleActivity(DetailActivity.class);
     }
 }
