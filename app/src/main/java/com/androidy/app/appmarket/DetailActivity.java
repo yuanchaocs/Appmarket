d74327e84ee4a8378bf16819f727150b8a35ccd9
袁小超 <yuanchaocs@163.com>
2015-12-13 02:30:02 +0800

    测试提交

--- app/src/main/java/com/androidy/app/appmarket/DetailActivity.java
+++ app/src/main/java/com/androidy/app/appmarket/DetailActivity.java
@@ -1,5 +1,7 @@
 package com.androidy.app.appmarket;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,5 +20,23 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void share(String content, Uri uri) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        if (uri != null) {
+            //uri 是图片的地址
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+            shareIntent.setType("image/*");
+            //当用户选择短信时使用sms_body取得文字
+            shareIntent.putExtra("sms_body", content);
+        } else {
+            shareIntent.setType("text/plain");
+        }
+        shareIntent.putExtra(Intent.EXTRA_TEXT, content);
+        //自定义选择框的标题
+        //startActivity(Intent.createChooser(shareIntent, "邀请好友"));
+        //系统默认标题
+        startActivity(shareIntent);
     }
 }
